3fe187e0a18b4f4cfb5082d8d8341c2a14f347e8
Adarsh tripathi <adarshtripathi9793085@gmail.com>
2026-01-05 23:24:03 +0530

    Add files via upload

---
+++ string4.py
@@ -0,0 +1,9 @@
+'''a = "format() method"
+b = "This is an example of {} method.".format(a)
+print(b)'''
+
+a = "apple"
+b = "banana"
+c = "cherry"
+string = "{0} {1} {2}".format(a, c, b)
+print(string)
